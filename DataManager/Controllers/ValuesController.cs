64d1c99b5810387a5501a8bda2369b347ac680bf
Fatih <mfatihinan@gmail.com>
2020-11-21 22:26:33 -0500

    Data manager project added

---
+++ DataManager/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Identity;
+using System.Web.Http;
+
+namespace DataManager.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IHttpActionResult Get()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            return Ok(new string[] { "value1", "value2", userId });
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
