807972846c44b1a5cd7768c8d130c756f563b6dc
Fatih <mfatihinan@gmail.com>
2021-02-15 21:33:43 -0500

    Getting user data is done

---
+++ DataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,17 @@
+﻿using DataManager.Library.Internal.DataAccess;
+using DataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace DataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+            var p = new { Id = Id };
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "Data");
+            return output;
+        }
+    }
+}
---
+++ DataManager/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using DataManager.Library.DataAccess;
+using DataManager.Library.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DataManager.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+            return data.GetUserById(userId);
+        }
+    }
+}
