0604bf16a23b3c0d4ea291d04f6d6bbd6ec2bfc6
Fatih <mfatihinan@gmail.com>
2021-05-30 00:29:43 -0400

    Add to Cart working

--- RMWPFDesktopUI/ViewModels/SalesViewModel.cs
+++ RMWPFDesktopUI/ViewModels/SalesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Micro;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 using WPFDesktopUI.Library.Api;
 using WPFDesktopUI.Library.Models;
@@ -36,9 +37,21 @@ namespace WPFDesktopUI.ViewModels
                 NotifyOfPropertyChange(() => Products);
             }
         }
-        private BindingList<string> _cart;
 
-        public BindingList<string> Cart
+        private ProductModel _selectedProduct;
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -48,8 +61,7 @@ namespace WPFDesktopUI.ViewModels
             }
         }
 
-        private int _itemQuantity;
-
+        private int _itemQuantity = 1;
         public int ItemQuantity
         {
             get { return _itemQuantity; }
@@ -57,6 +69,7 @@ namespace WPFDesktopUI.ViewModels
             {
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
@@ -64,8 +77,12 @@ namespace WPFDesktopUI.ViewModels
         {
             get
             {
-                // TODO: Replace with calculation
-                return "$0.00";
+                decimal subTotal = 0;
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+                return subTotal.ToString("C");
             }
         }
 
@@ -92,17 +109,36 @@ namespace WPFDesktopUI.ViewModels
             get
             {
                 bool output = false;
-
-                // Make sure something is selected
-                //Makse sre there is an item quatity
-
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
                 return output;
             }
         }
 
         public void AddToCart()
         {
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
 
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
         }
         public bool CanRemoveFromCart
         {
@@ -118,7 +154,7 @@ namespace WPFDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
-
+            NotifyOfPropertyChange(() => SubTotal);
         }
         public bool CanCheckOut
         {
---
+++ WPFDesktopUI.Library/Models/CartItemModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WPFDesktopUI.Library.Models
+{
+    public class CartItemModel
+    {
+        public ProductModel Product { get; set; }
+        public int QuantityInCart { get; set; }
+        public string DisplayText
+        {
+            get
+            {
+                return $"{Product.ProductName} ({QuantityInCart})";
+            }
+        }
+    }
+}
