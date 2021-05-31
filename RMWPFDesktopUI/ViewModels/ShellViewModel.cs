985fd099bcff6dd9a80b6390750bfe6c109b2d28
Fatih <mfatihinan@gmail.com>
2021-05-30 21:02:42 -0400

    Some refactoring and enhancements

--- RMWPFDesktopUI/ViewModels/ShellViewModel.cs
+++ RMWPFDesktopUI/ViewModels/ShellViewModel.cs
@@ -13,20 +13,17 @@ namespace WPFDesktopUI.ViewModels
 
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
 
         public ShellViewModel(
             IEventAggregator events,
-            SalesViewModel salesVM,
-            SimpleContainer container)
+            SalesViewModel salesVM)
         {
             _events = events;
             _salesVM = salesVM;
-            _container = container;
 
             _events.Subscribe(this);
 
-            ActivateItem(_container.GetInstance<LoginViewModel>());
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogOnEvent message)
